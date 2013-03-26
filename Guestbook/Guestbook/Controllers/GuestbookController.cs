cd24f2f136b2fe421d84bc6d5d28ec8d9d57f75e
Vinícius Aragão <viniciusaragao@gmail.com>
2013-03-26 11:12:11 -0300

    Inclusão de classes

---
+++ Guestbook/Guestbook/Controllers/GuestbookController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Guestbook.Models;
+
+namespace Guestbook.Controllers
+{
+    public class GuestbookController : Controller
+    {
+
+        private GuestbookContext _db = new GuestbookContext();
+
+        public ActionResult Index()
+        {
+            var mostRecentEntries =
+            (from entry in _db.Entries
+             orderby entry.DateAdded descending
+             select entry).Take(20);
+             ViewBag.Entries = mostRecentEntries.ToList();
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(GuestbookEntry entry)
+        {
+            entry.DateAdded = DateTime.Now;
+
+            _db.Entries.Add(entry);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Guestbook/Guestbook/Models/GuestbookEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Guestbook.Models
+{
+    public class GuestbookEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+
+    public class GuestbookContext : DbContext
+    {
+        public GuestbookContext()
+            : base("Guestbook")
+        {
+        }
+        public DbSet<GuestbookEntry> Entries { get; set; }
+    }
+    
+}
